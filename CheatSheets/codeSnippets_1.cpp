212929a0cf243f92ee2b7512d0628193606b032b
lws803 <lws803@gmail.com>
2018-01-18 22:59:18 +0800

    Initiated code snippets

---
+++ CheatSheets/codeSnippets_1.cpp
@@ -0,0 +1,15 @@
+
+// To get input of different cases and move on to the next test case on newline
+
+void getInput () {
+	int num;
+	while(true) {
+		cin >> num;
+		// Perform your operations here
+
+		if (cin.peek() == '\n') 
+			break;
+	}
+}
+
+
