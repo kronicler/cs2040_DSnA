5c6b7546900793152fc3d145fed0664a8e140a8c
lws803 <lws803@gmail.com>
2018-02-12 20:14:34 +0800

    Increased searching speed by reducing visited vector to a multidim array

--- KattisPractices/wilson/10_kinds_of_people.cpp
+++ KattisPractices/wilson/10_kinds_of_people.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 char mtx[1000][1000];
 
+/**
 int visit_check (int x, int y, vector<tuple<int, int>> visited) {
     for (auto it = visited.begin(); it != visited.end(); it++) {
         if (x == get<0>(*it) && y == get<1>(*it))
@@ -16,28 +17,28 @@ int visit_check (int x, int y, vector<tuple<int, int>> visited) {
     }
     return 0;
 }
+*/
 
-
-void search_neighbours (vector<tuple<int, int>> visited, stack<tuple<int, int>> *toVist,tuple<int, int> current, int row, int col) {
+void search_neighbours (int visited[][1000], stack<tuple<int, int>> *toVist,tuple<int, int> current, int row, int col) {
     int x = get<0>(current);
     int y = get<1>(current);
     char currentChar = mtx[y][x];
     
-    if (x+1 < col && mtx[y][x+1] == currentChar && !visit_check(x+1, y, visited)) {
+    if (x+1 < col && mtx[y][x+1] == currentChar && visited[y][x+1] != 1) {
         auto newCoords = make_tuple(x+1, y);
         toVist->push(newCoords);
     }
-    if (x-1 >= 0 && mtx[y][x-1] == currentChar && !visit_check(x-1, y, visited)) {
+    if (x-1 >= 0 && mtx[y][x-1] == currentChar && visited[y][x-1] != 1) {
         auto newCoords = make_tuple(x-1, y);
         toVist->push(newCoords);
         
     }
-    if (y-1 >= 0 && mtx[y-1][x] == currentChar && !visit_check(x, y-1, visited)) {
+    if (y-1 >= 0 && mtx[y-1][x] == currentChar && visited[y-1][x] != 1) {
         auto newCoords = make_tuple(x, y-1);
         toVist->push(newCoords);
         
     }
-    if (y+1 < row && mtx[y+1][x] == currentChar && !visit_check(x, y+1, visited)) {
+    if (y+1 < row && mtx[y+1][x] == currentChar && visited[y+1][x] != 1) {
         auto newCoords = make_tuple(x, y+1);
         toVist->push(newCoords);
         
@@ -47,19 +48,23 @@ void search_neighbours (vector<tuple<int, int>> visited, stack<tuple<int, int>> 
 
 int dfs (int x1, int y1, int x2, int y2, int row, int col) {
     auto current = make_tuple(x1, y1);
-    vector<tuple<int, int>> visited;
+    //vector<tuple<int, int>> visited;
     stack<tuple<int, int>> toVisit;
+    int visited [1000][1000] = {{0}};
     toVisit.push(current);
     while (!toVisit.empty()) {
         current = toVisit.top();
         toVisit.pop();
-        visited.push_back(current);
+        //visited.push_back(current);
+        visited[get<1>(current)][get<0>(current)] = 1;
         if (get<0>(current) == x2 && get<1>(current) == y2) return 1; // found the end
         search_neighbours(visited, &toVisit ,current, row, col);
     }
     return 0;
 }
 
+
+// TODO: Find a faster method to solve this
 
 
 int main () {
