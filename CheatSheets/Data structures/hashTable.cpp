260e1cef29cd6ea8bbf624ac0534dfbf3d6ed737
lws803 <lws803@gmail.com>
2018-03-09 10:42:39 +0800

    Added load factor calculation for hash tables

--- CheatSheets/Data structures/hashTable.cpp
+++ CheatSheets/Data structures/hashTable.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+
+using namespace std;
+
+
 class HT_direct {
 private:
     string arr[1000];
@@ -23,7 +31,7 @@ public:
 
 class HT_seperate {
 private:
-    int size; // Size its best to keep it to prime numbers 
+    int size; // Size its best to keep it to prime numbers
     list<int> arr[1000]; // This must be a doubly linked list
     
     
@@ -61,22 +69,24 @@ public:
             arr[base].erase(it);
         }
     }
+    
+    double loadFactor() {return (double)size/1000;} // 1000 because array size is 1000
 };
 
 
 class HT_open {
     
-// Precond: All integers are positive, -1 is used to signify empty slot, -2 is used to signify deleted slot
-// Switch the probing style to your liking i.e uncommment it
+    // Precond: All integers are positive, -1 is used to signify empty slot, -2 is used to signify deleted slot
+    // Switch the probing style to your liking i.e uncommment it
     
 private:
     int arr[1000] = {0};
-    int size; // Size its best to keep it to prime numbers 
-    int threshold;
+    int size; // Size its best to keep it to prime numbers
+    
 public:
-    HT_open (int input, int input2) {
+    HT_open (int input) {
         size = input;
-        threshold = input2;
+        
         for (int i = 0; i < size; i++) {
             arr[i] = -1; // init to all -1
         }
@@ -89,10 +99,10 @@ public:
             int i = (base * step) % size;
             /*
              int i = (base + step*step) % size; // Quadratic probing
-            
+             
              int base2 = data%SOMETHING;
              int i = (base + step * base2) % size;
-            */
+             */
             
             if (arr[i] == -1 || arr[i] == -2) {
                 arr[i] = data;
@@ -100,7 +110,6 @@ public:
             } else {
                 step++; // to find the next best spot
             }
-            if (step > threshold) break;
         }
     }
     
@@ -115,8 +124,8 @@ public:
              
              int base2 = data%SOMETHING;
              int i = (base + step * base2) % size;
-            */
-
+             */
+            
             
             if (arr[i] != -1) {
                 if (arr[i] == data)
@@ -125,8 +134,6 @@ public:
             } else {
                 return false; // not found
             }
-            
-            if (step > threshold) break;
         }
         return false;
     }
@@ -143,20 +150,18 @@ public:
              
              int base2 = data%SOMETHING;
              int i = (base + step * base2) % size;
-            */
-
+             */
+            
             if (arr[i] == data) {
                 arr[i] = -2;
                 break;
             }else {
                 step++;
             }
-            
-            if (step > threshold) break;
         }
     }
     
-    
+    double loadFactor() {return (double)size/1000;} // 1000 because array size is 1000
 };
 
 
@@ -172,4 +177,4 @@ public:
  Minimize clustering,
  Give different probe sequences when 2 different keys collide,
  Fast, O(1).
-*/
+ */
