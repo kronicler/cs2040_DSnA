66e66d58e0ac1fc6bd74eacb51127839907969fb
lws803 <lws803@gmail.com>
2018-03-14 12:32:46 +0800

    Added convo log and custom comparator functions

--- CheatSheets/codeSnippets_2.cpp
+++ CheatSheets/codeSnippets_2.cpp
@@ -62,3 +62,50 @@ int main () {
     }
 }
 
+
+
+// Custom comparator functions 
+
+// Create the comparator function
+// This will only compare the first value of the pairs
+bool comparePairs (const pair<int, int> &lhs, pair<int, int> &rhs) {
+    return lhs.first > rhs.first;
+}
+
+// Create the comparator function for sets
+class compare {
+public:
+    bool operator()(const int lhs,const int rhs) const {
+        return lhs > rhs; // When this is true, it will trigger the sort/ swap
+    }
+};
+
+// Note that we cannot compare by the second of map similar to sets
+class compare_map {
+public:
+    bool operator()(const int lhs,const int rhs) const {
+        return lhs > rhs;
+    }
+};
+
+// Compare only second value of PQ
+class compare_pq {
+public:
+    bool operator()(const pair<int, int> lhs,const pair<int, int> rhs) const {
+        return lhs.second > rhs.second;
+    }
+};
+
+
+int main () {
+    // Syntax 
+    vector<pair<int, int > > test;
+    set<int, compare> test_set;
+    map<int, int, compare_map> test_map;
+    priority_queue<pair<int, int>, vector<pair<int, int>> , compare_pq> test_pq;
+
+}
+
+
+
+
