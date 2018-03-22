16cbddbd9ae451821a649d1817d84a0d2599ff54
lws803 <lws803@gmail.com>
2018-03-22 23:44:48 +0800

    Added convo log and made some changes to compound words

--- KattisPractices/wilson/compound_words_ON.cpp
+++ KattisPractices/wilson/compound_words_ON.cpp
@@ -1,20 +1,28 @@
-
 #include <stdio.h>
 #include <iostream>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
 int main () {
-    
     set<string> input;
+    set<string> input3;
+    /*
+    input.insert("a");
+    input.insert("ab");
+    input.insert("c");
+    */
+    
     /*
     input.insert("a");
     input.insert("bb");
     input.insert("ab");
     input.insert("b");
-    */
+     */
     
+    
+    // TODO: Find out how to ignore cases when abb can happen because of a + bb or ab + b
     string in;
     //cin.ignore();
     while (cin >> in) {
@@ -22,18 +30,32 @@ int main () {
             input.insert(in);
         }
     }
+    
+    
+    
     for (auto it = input.begin(); it != input.end(); it++) {
         for (auto it2 = input.begin(); it2 != input.end(); it2++) {
             if (it != it2) {
                 if (next(it) != input.end() && (*it + *it2 >= *next(it) + *input.begin())) {
-                    break;
+                    input3.insert(*it + *it2);
+                }else {
+                    
+                    if (!input3.empty() && *it + *it2 >= *input3.begin()) {
+                        input3.insert(*it + *it2);
+                        
+                        while (!input3.empty()) {
+                            cout << *input3.begin() << endl;
+                            input3.erase(input3.begin());
+
+                        }
+                    } else {
+                        cout << (*it + *it2) << endl;
+                    }
                 }
-                cout << (*it + *it2) << endl;
             }
         }
-
     }
-    
+    // TODO: Find out what other test cases could break this 
     
 }
 
--- KattisPractices/wilson/convo_log.cpp
+++ KattisPractices/wilson/convo_log.cpp
@@ -1,65 +1,47 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
-#include <map>
+#include <vector>
 #include <sstream>
 #include <algorithm>
 
 using namespace std;
 
-
 int main () {
-    
-    int numlines;
-    cin >> numlines;
-    unordered_map<string,  unordered_set<string>> name_word;
-    unordered_map<string, int > words;
-    
+    int numlines; cin >> numlines;
+    vector<pair<int, string>> words;
+    unordered_map<string, int> mymap;
+    unordered_set<string> names;
+
     for (int i = 0; i < numlines; i++) {
         string input;
         string name;
         cin >> name;
+        names.insert(name);
         cin.ignore();
         getline(cin, input);
         
         istringstream iss (input);
         string token;
         
-        
         while (getline(iss, token, ' '))
         {
-            words[token]++;
-            name_word[name].insert(token);
+            mymap[token]++;
         }
     }
     
-    vector<pair<int, string>> to_be_sorted;
     
-    for (auto it = words.begin(); it != words.end(); it++) {
-        int count = 0;
-        for (auto it2 = name_word.begin(); it2 != name_word.end(); it2++) {
-            // For each name
-            auto it3 = it2->second.find(it->first); // Find the word
-            if (it3 != it2->second.end()) count++;
-        }
-        if (count == name_word.size()) {
-            // All users have spoke it
-            to_be_sorted.push_back(make_pair(it->second, it->first));
+    for (auto it : mymap) {
+        if (it.second >= names.size()) {
+            words.push_back(make_pair(it.second, it.first));
         }
     }
     
-    if (to_be_sorted.empty()) {
-        cout << "ALL CLEAR" << endl;
-        return 0;
-    }
-    
-    sort(to_be_sorted.begin(), to_be_sorted.end());
-    reverse(to_be_sorted.begin(), to_be_sorted.end());
-    for (auto it  = to_be_sorted.begin(); it != to_be_sorted.end(); it++) {
+    for (auto it = words.rbegin(); it != words.rend(); it++) {
         cout << it->second << endl;
     }
     
+    if (words.empty()) cout << "ALL CLEAR" << endl;
 }
 
