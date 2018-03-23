9766ea51dd16568dbb9cd2a69c76f44fe3e58bc1
lws803 <lws803@gmail.com>
2018-03-24 00:52:33 +0800

    Added new BST_AVL with ability to store duplicates

--- KattisPractices/wilson/bst.cpp
+++ KattisPractices/wilson/bst.cpp
@@ -5,38 +5,56 @@
 
 using namespace std;
 
-unordered_map<int, int> myMap;
-unordered_map<int, int> myHeight;
+unordered_map<int, int> myHeight; // key to height pair
+
+int parents [300000] = {0};
+pair<int, int> left_parent [300000] ; // First is the key itself, second is the index in the parents arr
+pair<int, int> right_parent [300000] ;
 
 
+int curr_index = 0;
+
+
+
+// TODO: Implement 3 arrays where arr_right, left and parent will share the same index
+// or implement an unordered map where they will share the same index too
 int insert (int key) {
-    if (myMap.empty()) {
-        myMap[0] = key;
-        myHeight[0] = 0;
+    if (curr_index == 0) {
+
+        parents[curr_index++] = key;
+        myHeight[key] = 0;
         return 0 ;
     }
     
     int index = 0;
+    // This takes too many traversals 
     while (true) {
-        if (key > myMap[index]) {
+        if (key > parents[index]) {
             // traverse right
-            if (myMap[2*index + 2] == 0) {
+            if (right_parent[index].first == 0) {
                 // found insertion pt
-                myMap[2*index + 2] = key;
-                myHeight[2*index + 2] = myHeight[index] + 1;
-                return myHeight[2*index + 2];
+                right_parent[index] = make_pair(key, curr_index);
+                parents[curr_index++] = key;
+                
+                myHeight[key] = myHeight[parents[index]] + 1;
+                return myHeight[key];
             }
-            index = 2*index+2;
+            // TODO: Find a way to bridge this part, to move to the next parent
+            index = right_parent[index].second;
+            //cout << index << endl;
         }
-        else if (key < myMap[index]) {
+        // TODO: continue conversion
+        else if (key < parents[index]) {
             // traverse left
-            if (myMap[2*index + 1] == 0) {
+            if (left_parent[index].first == 0) {
                 // found insertion pt
-                myMap[2*index + 1] = key;
-                myHeight[2*index + 1] = myHeight[index] + 1;
-                return myHeight[2*index + 1];
+                left_parent[index] = make_pair(key, curr_index);
+                parents[curr_index++] = key;
+                myHeight[key] = myHeight[parents[index]] + 1;
+                return myHeight[key];
             }
-            index = 2*index+1;
+            index = left_parent[index].second;
+            //cout << index << endl;
         }
         else{
             return 0;
@@ -51,10 +69,9 @@ int main () {
     int count = 0;
     int num; cin >> num;
     for (int i = 1; i <= num; i++) {
-        //int key; cin >> key;
-        count += insert(i);
+        int key; cin >> key;
+        count += insert(key);
         cout << count << endl;
     }
-    // TODO: Find out how to minimise the index size as it overflows too easily 
-    
+
 }
