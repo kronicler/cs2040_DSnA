4f57f64aee45375c3d4b100d12abec3f49d9484b
lws803 <lws803@gmail.com>
2018-05-09 16:41:25 +0800

    Added LCA for BST

--- CheatSheets/Data structures/BST_AVL.cpp
+++ CheatSheets/Data structures/BST_AVL.cpp
@@ -226,6 +226,19 @@ protected:
     }
 
 
+    BSTVertex * lca (BSTVertex * root, int n1, int n2) {
+        if (root == NULL) return NULL;
+        // If both n1 and n2 are smaller than root, then LCA lies in left
+        if (root->data > n1 && root->data > n2)
+            return lca(root->left, n1, n2);
+     
+        // If both n1 and n2 are greater than root, then LCA lies in right
+        if (root->data < n1 && root->data < n2)
+            return lca(root->right, n1, n2);
+     
+        return root;
+    }
+
     
 public:
     BST() { root = NULL; }
@@ -295,6 +308,12 @@ public:
 
     int select (int rank) {
         return select(root, rank)->key;
+    }
+
+
+    int lca_handler (int n1, int n2) {
+        BSTVertex * curr = lca(root, n1, n2);
+        return curr->key;
     }
 
 };
